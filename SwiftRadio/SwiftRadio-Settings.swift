a859d37b24a4acb9b2c065088f90844bf8b4ed22
pfriedman <psfriedman@gmail.com>
2018-04-27 11:22:19 -0700

    initial commit

---
+++ SwiftRadio/SwiftRadio-Settings.swift
@@ -0,0 +1,40 @@
+//
+//  SwiftRadio-Settings.swift
+//  Swift Radio
+//
+
+import Foundation
+
+//**************************************
+// GENERAL SETTINGS
+//**************************************
+
+// Display Comments
+let kDebugLog = true
+
+//**************************************
+// STATION JSON
+//**************************************
+
+// If this is set to "true", it will use the JSON file in the app
+// Set it to "false" to use the JSON file at the stationDataURL
+
+let useLocalStations = true
+let stationDataURL   = "http://yoururl.com/json/stations.json"
+
+//**************************************
+// SEARCH BAR
+//**************************************
+
+// Set this to "true" to enable the search bar
+let searchable = false
+
+//**************************************
+// NEXT / PREVIOUS BUTTONS
+//**************************************
+
+// Set this to "false" to show the next/previous player buttons
+let hideNextPreviousButtons = true
+
+let apiKey = "7eedb5cf94144bc97037c8c13e67c235"
+let apiSecret = "8b3ae6bd5706fd830ff9ccdca04f17c0"
